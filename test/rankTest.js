122d28de269eccc2d2faf6ed2feb42b8da3f8a30
MandyChen <497133212@qq.com>
2020-08-27 19:36:18 +0800

    [rank][mandy] test : add voyage.zone is china ,and history include china and history.length>8 and voyage.length>14

--- test/rankTest.js
+++ test/rankTest.js
@@ -353,3 +353,45 @@ rankTest('voyage.zone is china ,and history include china and history.length>8 a
   const result = voyageProfitFactor(voyage,history);
   t.is(result, 7);
 })
+rankTest('voyage.zone is china ,and history include china and history.length>8 and voyage.length>14', t => {
+  const voyage = {
+    zone: 'china',
+    length: 15,
+  };
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },
+    {
+      zone: 'china',
+      profit: 7,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },
+    {
+      zone: 'china',
+      profit: 7,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },
+    {
+      zone: 'china',
+      profit: 7,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },
+    {
+      zone: 'china',
+      profit: 7,
+    },
+  ];
+  const result = voyageProfitFactor(voyage,history);
+  t.is(result, 7);
+})
