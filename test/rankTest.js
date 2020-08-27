80796fada6d07f62c4e4e3c1e235f952af9238df
MandyChen <497133212@qq.com>
2020-08-27 19:25:42 +0800

    [rank][mandy] test : add voyage.zone not china ,and history not include china and history.length<=8 and voyage.length<=1414

--- test/rankTest.js
+++ test/rankTest.js
@@ -183,3 +183,25 @@ rankTest('voyage.zone is china ,and history not include china and history.length
   const result = captainHistoryRisk(voyage,history);
   t.is(result, 5);
 })
+
+rankTest('voyage.zone not china ,and history not include china and history.length<=8 and voyage.length<=14', t => {
+  const voyage = {
+    zone: 'test',
+    length: 14,
+  };
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },
+    {
+      zone: 'test',
+      profit: 7,
+    },
+  ];
+  const result = captainHistoryRisk(voyage,history);
+  t.is(result, 5);
+})
