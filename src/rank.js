fff6e3cd9c5d913f8e0c6cec6c861a9aaed03cbf
MandyChen <497133212@qq.com>
2020-08-27 20:34:06 +0800

    [rank][mandy] refactor : extract the zoneIsChinaAndHasChina and NotHasChina

--- src/rank.js
+++ src/rank.js
@@ -34,31 +34,40 @@ function captainHistoryRisk(voyage, history) {
 
 function voyageProfitFactor(voyage, history) {
   let result = 2;
-  if (voyage.zone === 'china' ||voyage.zone === 'east-indies' ) {
+  if (voyage.zone === 'china' || voyage.zone === 'east-indies') {
     result += 1;
   }
- 
+
   if (voyage.zone === 'china' && hasChina(history)) {
-    result += 3;
-    if (history.length > 10) {
-      result += 1;
-    }
-    if (voyage.length > 12) {
-      result += 1;
-    }
-    if (voyage.length > 18) {
-      result -= 1;
-    }
+    result = zoneIsChinaAndHasChina(result, history, voyage);
   }
   else {
-    if (history.length > 8) {
-      result += 1;
-    }
-    if (voyage.length > 14) {
-      result -= 1;
-    }
+    result = NotHasChina(history, result, voyage);
   }
+  return result;
+}
 
+function NotHasChina(history, result, voyage) {
+  if (history.length > 8) {
+    result += 1;
+  }
+  if (voyage.length > 14) {
+    result -= 1;
+  }
+  return result;
+}
+
+function zoneIsChinaAndHasChina(result, history, voyage) {
+  result += 3;
+  if (history.length > 10) {
+    result += 1;
+  }
+  if (voyage.length > 12) {
+    result += 1;
+  }
+  if (voyage.length > 18) {
+    result -= 1;
+  }
   return result;
 }
 
@@ -66,12 +75,12 @@ function rating(voyage, history) {
   const vpf = voyageProfitFactor(voyage, history);
   const vr = voyageRisk(voyage);
   const chr = captainHistoryRisk(voyage, history);
-  let result="";
+  let result = "";
   if (vpf * 3 > (vr + chr * 2)) {
-    result= 'A';
+    result = 'A';
   }
   else {
-    result= 'B';
+    result = 'B';
   }
   return result;
 }
