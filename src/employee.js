b1e555cab8d6683ceb7673783dc2c89f5420a39e
MandyChen <497133212@qq.com>
2020-08-28 18:39:05 +0800

    [employee][mandy] refactor : if to switch

--- src/employee.js
+++ src/employee.js
@@ -1,26 +1,36 @@
 class Employee {
   constructor (name, type) {
-    this.validateType(type);
+    this.validateType(type)
     this._name = name;
     this._type = type;
   }
 
-  validateType (type) {
-    const validTypes = [
-      'engineer',
-      'manager',
-      'salesman',
-    ];
-    if (!validTypes.includes(type)) {
-      throw new Error(`Employee cannot be of type ${type}`);
+   validateType (type) {
+    switch (type) {
+      case "engineer":
+        return engineerType();
+      case "manager":
+        return  salesmanType();
+      case "salesman":
+        return  managerType();
+      default:
+        throw new Error(`Employee cannot be of type ${type}`);
     }
   }
-
+  
   toString () {
     return `${this._name} (${this._type})`;
   }
 }
-
 module.exports = {
   Employee,
 };
+function engineerType() {
+    return "engineer";
+}
+function salesmanType() {
+  return "salesman";
+}
+function managerType() {
+  return "manager";
+}
